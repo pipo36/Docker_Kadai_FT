e2a37e9452fac0a7df06af79d7710824886d18c0
Fukui <>
2025-08-30 12:37:59 +0900

    PHPを使いDBにアクセスまで来た

--- dokcer_kadai/htdocs/kadai.php
+++ dokcer_kadai/htdocs/kadai.php
@@ -1,3 +1,43 @@
-<?php 
-print("kadai.php OK!");
+<?php
+// データベースへ接続するために必要な情報
+// ホストはDBコンテナ
+$host = 'mysql';
+// mysql接続用のユーザー
+$username = 'data_kadai';
+$password = 'data';
+$database = 'kadai_master';
+
+// データベースへ接続するためのクラス生成
+$mysql = new mysqli($host, $username, $password, $database);
+
+// 接続エラーの確認
+if ($mysql->connect_error) {
+    die("データベース接続エラー: " . $mysql->connect_error);
+}
+
+$sql = "SELECT * FROM character_table";
+// クエリの実行
+$result = $mysql->query($sql);
+
+// 結果の処理
+if ($result) {
+    if ($result->num_rows > 0) {
+        // 結果セットから行を取得して処理
+        while ($row = $result->fetch_assoc()) {
+            // 各行のデータを表示
+            echo "id: " . $row["id"] . ", name: " . $row["name"] . ", number: "
+             . $row["number"] . "<br>";
+        }
+    } else {
+        echo "該当するデータはありません。";
+    }
+    // 結果セットを解放
+    // $result->free();
+} else {
+    echo "クエリの実行に失敗しました: " . $mysql->error;
+}
+
+// データベース接続を閉じる
+$mysql->close();
+
 ?>
--- dokcer_kadai/mysql/initialize.sql
+++ dokcer_kadai/mysql/initialize.sql
@@ -19,3 +19,35 @@ CREATE TABLE IF NOT EXISTS kadai_table
 insert into kadai_table(id, name, number, message) value (1,"kadai1", 1, "message1");
 insert into kadai_table(id, name, number, message) value (2,"kadai2", 2, "message2");
 insert into kadai_table(id, name, number, message) value (3,"kadai3", 3, "message3");
+
+
+
+DROP DATABASE IF EXISTS kadai_master;
+CREATE DATABASE IF NOT EXISTS kadai_master;
+
+DROP TABLE IF EXISTS character_table;
+CREATE TABLE IF NOT EXISTS character_table
+(
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    number INT
+);
+
+insert into character_table(id, name, number) value (1, "tanaka", 1);
+insert into character_table(id, name, number) value (2, "tanaka", 2);
+insert into character_table(id, name, number) value (3, "tanaka", 3);
+
+DROP TABLE IF EXISTS class_table;
+CREATE TABLE IF NOT EXISTS class_table
+(
+    number INT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+insert into class_table(number, name) value (1, "Gold");
+insert into class_table(number, name) value (2, "Silver");
+insert into class_table(number, name) value (3, "Bronze");
+
+select * from character_table ch join class_table cl on ch.number = cl.number;
+
+select * from character_table union select * from class_table;
